6b9e8bc2fc788e8826fe371b09431bd4ea86bfd9
daylon <daylonvanwel@gmail.com>
2016-08-30 19:49:54 +0200

    new layout search for jobs

--- THOS/SearchForJobsViewController.swift
+++ THOS/SearchForJobsViewController.swift
@@ -12,7 +12,6 @@ class jobCell: UITableViewCell {
     
     @IBOutlet var userImageView: UIImageView!
     @IBOutlet var jobDescriptionLabel: UILabel!
-    @IBOutlet var jobPriceLabel: UILabel!
     
 }
 
@@ -24,21 +23,11 @@ class SearchForJobsViewController: UIViewController, UISearchBarDelegate, UITabl
     
     var jobsArray = [PFObject]()
     
-    var questionLabel: UILabel!
-    var outdoorHeroButton: UIButton!
-    var indoorHeroButton: UIButton!
+    var showCatagories: Bool!
+    var catagorieImage: UIImage!
+    
     var centerX: CGFloat!
     var centerY: CGFloat!
-    
-    var carpenterButton: UIButton!
-    var electricianButton: UIButton!
-    var tutorButton: UIButton!
-    
-    var gardenerButton: UIButton!
-    var jobsAroundTheHouseButton: UIButton!
-    var deliveryButton: UIButton!
-    
-    var backButton: UIButton!
     
     var jobTypeNumber: Int!
     var jobSubTypeNumber: Int!
@@ -48,41 +37,17 @@ class SearchForJobsViewController: UIViewController, UISearchBarDelegate, UITabl
         super.viewDidLoad()
         // setup default design
         
+        showCatagories = true
+        
         centerX = view.center.x
         centerY = view.center.y
-        
-//        NSNotificationCenter.defaultCenter().addObserverForName("openedWitdPushFromJobPoster", object: nil, queue: nil) { (notification: NSNotification) -> Void in
-//            
-//            self.openChatFromNotification(notification)
-//        }
-//
-//        NSNotificationCenter.defaultCenter().addObserverForName("userAppliedToJob", object: nil, queue: nil) { (notification: NSNotification) -> Void in
-//        
-//                    self.openChatFromNotification(notification)
-//        }
+
     }
     
     override func viewDidAppear(animated: Bool) {
         
         super.viewDidAppear(true)
-        
-        for subView in view.subviews {
-    
-            if subView.isKindOfClass(UILabel) || subView.isKindOfClass(UIButton) {
-            
-                subView.removeFromSuperview()
                 
-            }
-        }
-        
-        tableView.hidden = true
-        
-        self.checkForMewChats()
-        
-        self.tableView.hidden = true
-        self.jobsArray.removeAll(keepCapacity: true)
-        self.showJobOptions()
-
     }
 
 
@@ -91,187 +56,7 @@ class SearchForJobsViewController: UIViewController, UISearchBarDelegate, UITabl
     }
     
     
-    func showJobOptions() {
-        
-        questionLabel = UILabel(frame: CGRect(x: 10, y: 70, width: view.frame.size.width - 20, height: 60))
-        questionLabel.textColor = UIColor.ThosColor()
-        questionLabel.text = "Naar wat voor klus ben je op zoek?"
-        questionLabel.numberOfLines = 2
-        questionLabel.adjustsFontSizeToFitWidth = true
-        questionLabel.textAlignment = .Center
-        questionLabel.font = UIFont.systemFontOfSize(16, weight: UIFontWeightMedium)
-        
-        outdoorHeroButton = UIButton(frame: CGRect(x: 20, y: centerY - 30, width: view.frame.size.width - 40, height: 50))
-        outdoorHeroButton.backgroundColor = UIColor.ThosColor()
-        outdoorHeroButton.setTitle("Klussen buiten het huis", forState: .Normal)
-        outdoorHeroButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        outdoorHeroButton.addTarget(self, action: #selector(self.jobTypeButtonPressed(_:)), forControlEvents: .TouchUpInside)
-        outdoorHeroButton.tag = 0
-        
-        indoorHeroButton = UIButton(frame: CGRect(x: 20, y: centerY + 25, width: view.frame.size.width - 40, height: 50))
-        indoorHeroButton.backgroundColor = UIColor.ThosColor()
-        indoorHeroButton.setTitle("Klussen binnen het huis", forState: .Normal)
-        indoorHeroButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        indoorHeroButton.addTarget(self, action: #selector(self.jobTypeButtonPressed(_:)), forControlEvents: .TouchUpInside)
-        indoorHeroButton.tag = 1
-
-        backButton = UIButton(frame: CGRect(x: 10, y: view.frame.size.height - 100, width: 60, height: 60))
-        backButton.setTitle("Annuleer", forState: .Normal)
-        backButton.setTitleColor(UIColor.ThosColor(), forState: .Normal)
-        backButton.addTarget(self, action: #selector(self.backButtonPressed(_:)), forControlEvents: .TouchUpInside)
-        backButton.titleLabel?.adjustsFontSizeToFitWidth = true
-        backButton.hidden = true
-
-        view.addSubview(questionLabel)
-        view.addSubview(outdoorHeroButton)
-        view.addSubview(indoorHeroButton)
-        view.addSubview(backButton)
-        
-    }
-
-    func jobTypeButtonPressed(sender: UIButton)  {
-        
-        UIView.animateWithDuration(0.2, animations: {
-            
-            self.outdoorHeroButton.transform = CGAffineTransformMakeTranslation(-400, 0.1)
-            self.indoorHeroButton.transform = CGAffineTransformMakeTranslation(-400, 0.1)
-            
-        }) { (Bool) in
-            
-            self.outdoorHeroButton.removeFromSuperview()
-            self.indoorHeroButton.removeFromSuperview()
-            
-            if sender.tag == 0 {
-                
-                self.showOutdoorSubTypesOptions()
-                
-            } else if sender.tag == 1 {
-                
-                self.showIndoorSubTypesOptions()
-            }
-            
-            self.jobTypeNumber = sender.tag
-            self.backButton.hidden = false
-            
-        }
-        
-        
-    }
-
-    
-    func showOutdoorSubTypesOptions() {
-        
-        questionLabel.text = "Wat voor klus rondom het huis wil je doen?"
-        
-        gardenerButton = UIButton(frame: CGRect(x: 20, y: centerY - 80, width: view.frame.size.width - 40, height: 50))
-        gardenerButton.backgroundColor = UIColor.ThosColor()
-        gardenerButton.setTitle("Hovenier", forState: .Normal)
-        gardenerButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        gardenerButton.addTarget(self, action: #selector(self.subTypeButtonPressed(_:)), forControlEvents: .TouchUpInside)
-        gardenerButton.tag = 0
-        gardenerButton.transform = CGAffineTransformMakeTranslation(+400, 0.1)
-        
-        jobsAroundTheHouseButton = UIButton(frame: CGRect(x: 20, y: centerY - 25, width: view.frame.size.width - 40, height: 50))
-        jobsAroundTheHouseButton.backgroundColor = UIColor.ThosColor()
-        jobsAroundTheHouseButton.setTitle("Klussen aan het huis", forState: .Normal)
-        jobsAroundTheHouseButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        jobsAroundTheHouseButton.addTarget(self, action: #selector(self.subTypeButtonPressed(_:)), forControlEvents: .TouchUpInside)
-        jobsAroundTheHouseButton.tag = 1
-        jobsAroundTheHouseButton.transform = CGAffineTransformMakeTranslation(+400, 0.1)
-        
-        deliveryButton = UIButton(frame: CGRect(x: 20, y: centerY + 30, width: view.frame.size.width - 40, height: 50))
-        deliveryButton.backgroundColor = UIColor.ThosColor()
-        deliveryButton.setTitle("Ophaal/bezorg diensten", forState: .Normal)
-        deliveryButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        deliveryButton.addTarget(self, action: #selector(self.subTypeButtonPressed(_:)), forControlEvents: .TouchUpInside)
-        deliveryButton.tag = 2
-        deliveryButton.transform = CGAffineTransformMakeTranslation(+400, 0.1)
-        
-        view.addSubview(gardenerButton)
-        view.addSubview(jobsAroundTheHouseButton)
-        view.addSubview(deliveryButton)
-        
-        UIView.animateWithDuration(0.2, animations: {
-            
-            self.gardenerButton.transform = CGAffineTransformIdentity
-            self.jobsAroundTheHouseButton.transform = CGAffineTransformIdentity
-            self.deliveryButton.transform = CGAffineTransformIdentity
-            
-        })
-        
-        
-    }
-    
-    func showIndoorSubTypesOptions()  {
-        
-        questionLabel.text = "Wat voor klus in huis wil je doen?"
-        
-        carpenterButton = UIButton(frame: CGRect(x: 20, y: centerY - 80, width: view.frame.size.width - 40, height: 50))
-        carpenterButton.backgroundColor = UIColor.ThosColor()
-        carpenterButton.setTitle("Klusjes man", forState: .Normal)
-        carpenterButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        carpenterButton.addTarget(self, action: #selector(self.subTypeButtonPressed(_:)), forControlEvents: .TouchUpInside)
-        carpenterButton.tag = 0
-        carpenterButton.transform = CGAffineTransformMakeTranslation(+400, 0.1)
-        
-        electricianButton = UIButton(frame: CGRect(x: 20, y: centerY - 25, width: view.frame.size.width - 40, height: 50))
-        electricianButton.backgroundColor = UIColor.ThosColor()
-        electricianButton.setTitle("Elektricien", forState: .Normal)
-        electricianButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        electricianButton.addTarget(self, action: #selector(self.subTypeButtonPressed(_:)), forControlEvents: .TouchUpInside)
-        electricianButton.tag = 1
-        electricianButton.transform = CGAffineTransformMakeTranslation(+400, 0.1)
-        
-        tutorButton = UIButton(frame: CGRect(x: 20, y: centerY + 30, width: view.frame.size.width - 40, height: 50))
-        tutorButton.backgroundColor = UIColor.ThosColor()
-        tutorButton.setTitle("Bijles", forState: .Normal)
-        tutorButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        tutorButton.addTarget(self, action: #selector(self.subTypeButtonPressed(_:)), forControlEvents: .TouchUpInside)
-        tutorButton.tag = 2
-        tutorButton.transform = CGAffineTransformMakeTranslation(+400, 0.1)
-        
-        view.addSubview(carpenterButton)
-        view.addSubview(electricianButton)
-        view.addSubview(tutorButton)
-        
-        UIView.animateWithDuration(0.2, animations: {
-            
-            self.carpenterButton.transform = CGAffineTransformIdentity
-            self.electricianButton.transform = CGAffineTransformIdentity
-            self.tutorButton.transform = CGAffineTransformIdentity
-            
-        })
-        
-    }
-    
-    func subTypeButtonPressed(sender: UIButton)  {
-        
-        jobSubTypeNumber = sender.tag
-        
-        self.getJobs()
-        
-    }
-    
-    func backButtonPressed(sender: UIButton)  {
-        
-        for subView in view.subviews {
-            
-            if subView.isKindOfClass(UILabel) || subView.isKindOfClass(UIButton) {
-                
-                subView.removeFromSuperview()
-                
-            }
-        }
-        
-        self.showJobOptions()
-        
-        
-    }
-    
     func getJobs() {
-        
-        print(jobTypeNumber)
-        print(jobSubTypeNumber)
         
         let querie = PFQuery(className: "Job")
         querie.includeKey("user")
@@ -287,8 +72,6 @@ class SearchForJobsViewController: UIViewController, UISearchBarDelegate, UITabl
 
         }
         
-    
-        
         querie.findObjectsInBackgroundWithBlock { (jobs, error ) -> Void in
             
             if error != nil {
@@ -297,26 +80,14 @@ class SearchForJobsViewController: UIViewController, UISearchBarDelegate, UITabl
                 
             } else {
                 
-                if ((jobs?.count) != nil) {
-                    
-                    if self.questionLabel != nil {
-                        
-                        self.questionLabel.removeFromSuperview()
-                    }
-                    
-                    for subView in self.view.subviews {
-                        
-                        if subView.isKindOfClass(UIButton) {
-                            
-                            subView.removeFromSuperview()
-                        }
-                    }
+                if ((jobs?.count) > 0) {
+                  
+                    self.showCatagories = false
                     
                     for job in jobs! {
                         
                         self.jobsArray.append(job)
                         
-                        self.tableView.hidden = false
                         self.tableView.reloadData()
                         
                     }
@@ -328,12 +99,39 @@ class SearchForJobsViewController: UIViewController, UISearchBarDelegate, UITabl
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+       
+        if showCatagories == true {
+            
+            return 3
+
+        } else {
+            
+            return 1
+        }
+        
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        
-        return jobsArray.count
+        if showCatagories == true {
+            
+            if section == 0 {
+                
+                return 4
+                
+            } else if section == 1 {
+                
+                return 2
+                
+            } else {
+                
+                return 1
+            }
+
+        } else  {
+            
+            return jobsArray.count
+        }
         
     }
     
@@ -341,22 +139,71 @@ class SearchForJobsViewController: UIViewController, UISearchBarDelegate, UITabl
         
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! jobCell
         
-        let job = self.jobsArray[indexPath.row]
-        
-        cell.jobDescriptionLabel.text = job["jobDescription"] as? String
-        cell.jobPriceLabel.text = "€ \(job["price"])"
-        
-        let user = job["user"] as! PFUser
-        
-        let userImagefile = user["userImgage"] as! PFFile
-        userImagefile.getDataInBackgroundWithBlock { (data, error) -> Void in
-        
-            cell.userImageView.image = UIImage(data: data!)
-        
+        cell.userImageView.clipsToBounds = true
+       
+        if showCatagories == true {
+       
+                if indexPath.section == 0 {
+            
+                    if indexPath.row == 0 {
+                
+                        cell.jobDescriptionLabel.text = "Schoonmaak"
+                        cell.userImageView.image = UIImage(named: "IndoorCleaning")
+                
+                    } else if indexPath.row == 1 {
+                
+                        cell.jobDescriptionLabel.text = "Oppas"
+                        cell.userImageView.image = UIImage(named: "nanny")
+                
+                
+                    } else if indexPath.row == 2 {
+                
+                        cell.jobDescriptionLabel.text = "Houtwerk"
+                        cell.userImageView.image = UIImage(named: "woodWork")
+                
+                
+                    } else if indexPath.row == 3 {
+                
+                        cell.jobDescriptionLabel.text = "Electricien"
+                        cell.userImageView.image = UIImage(named: "electrician")
+                
+                
+                    }
+            
+                } else if indexPath.section == 1 {
+            
+                    if indexPath.row == 0 {
+                
+                        cell.jobDescriptionLabel.text = "Rondom het huis"
+                        cell.userImageView.image = UIImage(named: "aroundTheHouse")
+                    
+                    } else if indexPath.row == 1 {
+                
+                        cell.jobDescriptionLabel.text = "In de tuin"
+                        cell.userImageView.image = UIImage(named: "garden")
+                
+                
+                    }
+                    
+                } else if indexPath.section == 2 {
+            
+                    if indexPath.row == 0 {
+                
+                        cell.jobDescriptionLabel.text = "Vervoer en verzend"
+                        cell.userImageView.image = UIImage(named: "pickUp")
+                    }
+            }
+
+        } else if showCatagories == false {
+            
+            let job = self.jobsArray[indexPath.row]
+            
+            cell.jobDescriptionLabel.text = job["jobDescription"] as? String
+                        
+            cell.userImageView.image = catagorieImage
+            
         }
-        
-        cell.userImageView.layer.masksToBounds = true
-        cell.userImageView.layer.cornerRadius = 35
+
         
         return cell
 
@@ -364,7 +211,29 @@ class SearchForJobsViewController: UIViewController, UISearchBarDelegate, UITabl
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
-        performSegueWithIdentifier("searchToDetailSegue", sender: indexPath)
+        if showCatagories == true {
+            
+            let cell = tableView.cellForRowAtIndexPath(indexPath)
+            
+            for view in (cell?.contentView.subviews)! {
+                
+                if view.isKindOfClass(UIImageView) {
+                    
+                    let imageView = view as! UIImageView
+                    self.catagorieImage = imageView.image
+                }
+            }
+            
+            self.jobTypeNumber = indexPath.section
+            self.jobSubTypeNumber = indexPath.row
+            
+            self.getJobs()
+            
+        } else if showCatagories ==  false {
+            
+            performSegueWithIdentifier("searchToDetailSegue", sender: indexPath)
+
+        }
     }
    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -420,44 +289,11 @@ class SearchForJobsViewController: UIViewController, UISearchBarDelegate, UITabl
 
     @IBAction func resetSearchButtonPressed(sender: AnyObject) {
         
-        self.tableView.hidden = true
-        self.jobsArray.removeAll(keepCapacity: true)
-        self.showJobOptions()
-        questionLabel.removeFromSuperview()
+        jobsArray.removeAll(keepCapacity: true)
+        showCatagories = true
+        tableView.reloadData()
     }
     
-    func checkForMewChats() {
-        
-//        let tabBarController = self.parentViewController as! JobSeekerTabbarController
-//        tabBarController.checkForMewChats()
-        
-    }
 
-    func openChatFromNotification(notification: NSNotification) {
-        
-//        let query = PFQuery(className: "Job")
-//        query.whereKey("objectId", equalTo: notification.object as! String)
-//        query.getFirstObjectInBackgroundWithBlock { (object, error) in
-//            if error != nil {
-//                
-//                print(error)
-//                
-//            } else {
-//                                
-//                let storyBoard  = UIStoryboard(name: "Main", bundle: nil)
-//                
-//                let chatController = storyBoard.instantiateViewControllerWithIdentifier("appliedJobsViewcontroller") as! MyAppliedJobsChatViewController
-//                
-//                chatController.jobId = object!.objectId
-//                chatController.jobGeoPoint = object!.valueForKey("jobLocation") as! PFGeoPoint
-//                chatController.jobDescription = object!.valueForKey("jobDescription") as! String
-//                chatController.job = object!
-//                
-//                
-//                self.presentViewController(chatController, animated: true, completion: nil)
-//                
-//            }
-//        }
-    }
 
 }
